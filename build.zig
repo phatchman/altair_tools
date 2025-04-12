244a3a5461210e05664e3717a3241c65dcc12fb8
Paul Hatchman <phatchman@gmail.com>
2025-04-12 12:53:04 +1000

    Added multi-arch build

--- build.zig
+++ build.zig
@@ -1,119 +1,122 @@
+//! Build altaridsk for single or multiple targets
+//! use -Dmulti-target=true to buld for all targets
+//! Recommended build options are --release=safe --strip-exe
 const std = @import("std");
-
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
-
-    const lib_mod = b.addModule("altair_disk", .{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/lib.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const lib = b.addStaticLibrary(.{
-        .name = "altair_disk",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        //        .root_source_file = b.path("src/lib.zig"),
-        .root_module = lib_mod,
-    });
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
-    const exe = b.addExecutable(.{
-        .name = "altairdsk",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const zigcli = b.dependency("cli", .{ .target = target, .optimize = optimize });
-    exe.root_module.addImport("zig-cli", zigcli.module("zig-cli"));
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/tests.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
-
-    //    const exe_check = b.addExecutable(.{
-    //        .name = "foo",
-    //        .root_module = exe_mod,
-    //    });
-    // There is no `b.installArtifact(exe_check);` here.
-
-    // Finally we add the "check" step which will be detected
-    // by ZLS and automatically enable Build-On-Save.
-    // If you copy this into your `build.zig`, make sure to rename 'foo'
-    //    const check = b.step("check", "Check if altair_disk compiles");
-    //    check.dependOn(&exe_check.step);
-    //
+    // Build for all supported targets.
+    const build_multi_target = b.option(bool, "multi-target", "Build for windows, linux and macos") orelse false;
+    // Don't output a binary.
     const no_bin = b.option(bool, "no-bin", "skip emitting binary") orelse false;
-    if (no_bin) {
-        b.getInstallStep().dependOn(&exe.step);
-    } else {
-        b.installArtifact(exe);
+    // Strip debug symbols
+    const strip_debug_symbols = b.option(bool, "strip-exe", "Strip debugging information") orelse false;
+
+    // Add other supported targets here as required.
+    const all_targets = [_]std.Build.ResolvedTarget{
+        b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .windows }),
+        b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .linux }),
+        b.resolveTargetQuery(.{ .cpu_arch = .x86_64, .os_tag = .macos }),
+        b.resolveTargetQuery(.{ .cpu_arch = .arm, .os_tag = .linux }),
+        b.resolveTargetQuery(.{ .cpu_arch = .aarch64, .os_tag = .windows }),
+        b.resolveTargetQuery(.{ .cpu_arch = .aarch64, .os_tag = .linux }),
+        b.resolveTargetQuery(.{ .cpu_arch = .aarch64, .os_tag = .macos }),
+    };
+    // If compiling for single target (default) single_exe will be set to the compile step for altairdsk.
+    var single_exe: ?*std.Build.Step.Compile = null;
+    const single_target = [_]std.Build.ResolvedTarget{b.standardTargetOptions(.{})};
+
+    // Targets is list of targets we are building for.
+    const targets = if (!build_multi_target)
+        single_target[0..]
+    else
+        all_targets[0..];
+
+    const optimize = b.standardOptimizeOption(.{});
+    const run_step = b.step("run", "Run the app");
+    const test_step = b.step("test", "Run unit tests");
+
+    // For each target build the library and exe's
+    for (targets) |target| {
+        const lib_mod = b.addModule("altair_disk", .{
+            .root_source_file = b.path("src/lib.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const lib = b.addStaticLibrary(.{
+            .name = "altair_disk",
+            .root_module = lib_mod,
+        });
+        if (targets.len > 1) {
+            const install = b.addInstallArtifact(lib, .{
+                .dest_dir = .{ .override = .{
+                    .custom = std.fmt.allocPrint(b.allocator, "lib/{s}-{s}", .{
+                        @tagName(target.result.cpu.arch),
+                        @tagName(target.result.os.tag),
+                    }) catch unreachable,
+                } },
+            });
+            b.default_step.dependOn(&install.step);
+        } else {
+            b.installArtifact(lib);
+        }
+        const exe = b.addExecutable(.{
+            .name = "altairdsk",
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .strip = strip_debug_symbols,
+        });
+        const zigcli = b.dependency("cli", .{ .target = target, .optimize = optimize });
+        exe.root_module.addImport("zig-cli", zigcli.module("zig-cli"));
+        if (targets.len > 1) {
+            const install = b.addInstallArtifact(exe, .{
+                .dest_dir = .{ .override = .{
+                    .custom = std.fmt.allocPrint(b.allocator, "bin/{s}-{s}", .{
+                        @tagName(target.result.cpu.arch),
+                        @tagName(target.result.os.tag),
+                    }) catch unreachable,
+                } },
+            });
+            b.default_step.dependOn(&install.step);
+        } else {
+            single_exe = exe;
+            b.installArtifact(exe);
+        }
+    }
+    if (single_exe) |exe| {
+        // Add run and test commands, but only for sinlge arch builds.
+        const target = single_target[0];
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        run_step.dependOn(&run_cmd.step);
+
+        // Unit tests
+        const lib_unit_tests = b.addTest(.{
+            .root_source_file = b.path("src/tests.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+        const exe_unit_tests = b.addTest(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+        test_step.dependOn(&run_lib_unit_tests.step);
+        test_step.dependOn(&run_exe_unit_tests.step);
+
+        // Don't output binary. Used for Zig "build on save" feature.
+        // Which skips the LLVM emit so you can see buil errors more quickly.
+        if (no_bin) {
+            b.getInstallStep().dependOn(&exe.step);
+        } else {
+            b.installArtifact(exe);
+        }
     }
 }
